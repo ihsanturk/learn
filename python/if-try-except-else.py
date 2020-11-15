c077843035b34a00902368d682d1d8eaa4b0d153
ihsan <ihsanl@pm.me>
2020-11-15 12:49:36 +0300

    add: another test error

--- python/if-try-except-else.py
+++ python/if-try-except-else.py
@@ -5,8 +5,8 @@ includes = True
 
 def main():
 	if not includes: return
-	try: pass #raise TestException()
-	except TestException: print('test exception occured.')
+	try: pass #raise TestException() #raise Exception()
+	except TestException: print('-> test exception occured!')
 	else: print('Desired output.')
 
 if __name__ == '__main__':
